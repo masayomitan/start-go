deb86fa2a15d60a40a15cd3c226768b35da5416f
kawasaki <yomitanmasa3523@gmail.com>
2021-06-08 23:21:38 +0900

    repository系

--- api/delivery/allget.go
+++ api/delivery/allget.go
@@ -9,3 +9,23 @@ import(
 type todoAllGetHandler struct {
     todoUseCase domain.TodoUseCase
 }
+
+func NewTodoAllGetHandler(c *fiber.App, th domain.TodoUsecase) {
+	handler := &todoAllGetHandler{
+		todoUseCase: th,
+	}
+
+	c.Get("/todos", handler.AllGet)
+}
+
+func (h *todoAllGetHandler) AllGet(c *fiber.Ctx) error {
+	// UseCaseのAllGetを呼びだす
+	todos, err := h.todoUseCase.AllGet()
+	if err != nil {
+		c.Status(500)
+		return c.JSON(fiber.Map{
+			"message": "Internal Server Error",
+		})
+	}
+	return c.JSON(todos)
+}c
