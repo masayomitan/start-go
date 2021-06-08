990e8620f346807821d56d2644b5c63b7154f7b9
kawasaki <yomitanmasa3523@gmail.com>
2021-06-09 00:04:54 +0900

    use

---
+++ api/usecase/todo_usecase.go
@@ -0,0 +1,47 @@
+package usecase
+
+import(
+	"todo/domain"
+)
+
+type todoUsecase struct {
+	todoRepo domain.TodoRepository
+}
+
+func NewTodoUsecase( tr domain.TodoRepository ) domain.TodoUsecase {
+	return todoUsecase {
+		todoRepo: tr,
+	}
+}
+
+func (t * todoUsecase ) AllGet() ([]domain.Todo, error ){
+	todos, err := t.todoRepo.AllGet()
+	if err != nil{
+		return nil, err
+	}
+	return todos, nil
+}
+
+func (t *todoUsecase) StatusUpdate(id int) error {
+	err := t.todoRepo.StatusUpdate(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *todoUsecase) Store(todo domain.Todo) error {
+	err := t.todoRepo.Store(todo)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *todoUsecase) Delete(id int) error {
+	err := t.todoRepo.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
